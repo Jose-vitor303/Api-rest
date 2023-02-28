2d896c55016b4a4c023fdd6af96670597abf0f77
José Vitor Façanha da Silva <jvitorfacanha@outlook.com>
2023-02-28 15:13:13 -0300

    test: ajustando as rotas api

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -5,29 +5,29 @@ const app = require("../server.js")
 describe("GET /api/users", () => {
 
     test("should return a description to a single user", async () => {
-      const response = await request(app).get("/api/users/63fc0091ca93b9be05b47123");
+      const response = await request(app).get("/api/users/user/63fc0091ca93b9be05b47123");
       expect(response.statusCode).toBe(200);
       expect(response.body.singleUser.description).toBe('perto de casa');
     });
 
     test("should return all the users", async()=>{
      
-      const response = await request(app).get("/api/users")
+      const response = await request(app).get("/api/users/all")
       expect(response.statusCode).toBe(200);
       expect(response.body.user.length).toBeGreaterThan(0);
     })
 });
 
   
-describe("DELETE /api/users/:id", ()=>{
+describe("DELETE /api/users/delete/:id", ()=>{
 
   test("should delete a user", async()=>{
-      const response = await request(app).delete("/api/users/63fbfdafd57f4c320768941d")//Esse teste só ira dar certo na primeira, pois na segunda tentativa o usuário já vai ter sido deletado
+      const response = await request(app).delete("/api/users/delete/63fbfdafd57f4c320768941d")//Esse teste só ira dar certo na primeira, pois na segunda tentativa o usuário já vai ter sido deletado
       expect(response.statusCode).toBe(200)
   })
 
   test("should return a status code 404", async()=>{
-    const response = await request(app).delete("/api/users/63fbfdafd57f4c3skas");
+    const response = await request(app).delete("/api/users/delete/63fbfdafd57f4c3skas");
     if(!response){
       expect(response.statusCode).toBe(404)
     }
@@ -41,7 +41,7 @@ describe("DELETE /api/users/:id", ()=>{
 describe("PATCH /api/users/:id", ()=>{
 
   test("should update a user", async()=>{
-    const response = await request(app).patch("/api/users/63fc00195b2ac54345d1781d").send({
+    const response = await request(app).patch("/api/users/update/63fc00195b2ac54345d1781d").send({
       name : "Vitor",
       dob: "1997-04-09",
       address: "Rua certa",
@@ -59,7 +59,7 @@ describe("POST /api/users", ()=>{
 
   test("should create a new user", async()=>{
 
-      const response = await request(app).post("/api/users").send({
+      const response = await request(app).post("/api/users/create").send({
         name : "Silvia",
         dob: "1987-04-04",
         address: "Rua Alameda",
